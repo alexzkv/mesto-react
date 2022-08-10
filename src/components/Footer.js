db5a35202056e03f5e4bec66f017a7017c57d1a7
AleksandrZakharov58 <91503552+AleksandrZakharov58@users.noreply.github.com>
2022-08-10 18:21:11 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__title">© {new Date().getFullYear()} Александр Захаров</p>
-    </footer>
-  );
-}
